be2865f7a0b3020dc44036968d2ffbe936497b10
Alan Warren <alan.warren@gmail.com>
2022-10-14 09:50:03 -0400

    Create async-elements.js

---
+++ async-elements.js
@@ -0,0 +1,23 @@
+async function waitForElement(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
